88f6fd4d3b85c44db04efa24848d07495bea5532
Georg G <nilsding@nilsding.org>
2016-01-31 02:17:48 +0100

    formatting, fixed decoding

--- encdec.rb
+++ encdec.rb
@@ -1,47 +1,49 @@
 #!/bin/ruby
 
-require "json"
-require "pry"
+require 'json'
+require 'pry'
 
 def encode(buf)
-  get_block_start = JSON.parse(File.read("./get-block-start.json"))
+  get_block_start = JSON.parse(File.read('./get-block-start.json'))
   strs = []
   i = 0
   while i < buf.length
     b1 = buf[i]
     b2 = if i + 1 < buf.length then buf[i+1] else -1 end
-    codePoint = get_block_start[b2.to_s] + b1
-    str = [codePoint].pack("U*")
+    codepoint = get_block_start[b2.to_s] + b1
+    str = [codepoint].pack('U*')
     strs << str
     i += 2
   end
-  strs.join("")
+  strs.join('')
 end
 
 def decode(str)
-  get_b2 = JSON.parse(File.read("./get-b2.json"))
+  get_b2 = JSON.parse(File.read('./get-b2.json'))
   bufs = []
   done = false
   i = 0
-  codePoints = str.each_codepoint.to_a
+  codepoints = str.codepoints
   while i < str.length
-    codePoint = codePoints[i]
-    b1 = codePoint & ((1 << 8) -1)
-    b2 = get_b2[(codePoint - b1).to_s]
-    raise "Not a valid Base65536 code point: #{codePoint}" if b2 == nil
-    buf = if b2 == -1 then [b1].bytes else [b1, b2].bytes end
-    if buf.length == 1 and done then raise "Base65536 sequence continued after final byte" end
+    codepoint = codepoints[i]
+    b1 = codepoint & ((1 << 8) - 1)
+    b2 = get_b2[(codepoint - b1).to_s]
+    fail "Not a valid Base65536 code point: #{codepoint}" if b2 == nil
+    buf = b2 == -1 ? [b1].pack('U*') : [b1, b2].pack('U*')
+    if buf.length == 1
+      done and fail 'Base65536 sequence continued after final byte'
+      done = true
+    end
     bufs << buf
-    if codePoint >= (1 << 16) then i += 1 end
     i += 1
   end
-  bufs
+  bufs.join ''
 end
 
-#tecstFile = File.read("./testfile")
+#tecstFile = File.read('./testfile')
 #puts encode(testFile.bytes)
 
-testVal = encode("nightbug is about to drop some dank shit yo".bytes)
+testVal = encode('nightbug is about to drop some dank shit yo MEMEN'.bytes)
 puts testVal
 puts decode testVal
 
